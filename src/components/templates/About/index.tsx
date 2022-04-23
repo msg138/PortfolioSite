47be0d02a57acdcfd0a35f09456fbd38c63a4e0d
Michael George <michaelg@theonestone.com>
2022-04-22 19:28:55 -0700

    WIP: About page.

--- src/components/templates/About/index.tsx
+++ src/components/templates/About/index.tsx
@@ -1,10 +1,31 @@
 import React, { ReactElement } from 'react';
+import clsx from 'clsx';
+import * as AboutCSS from './About.module.scss';
 
 const About = (): ReactElement => {
   return (
-    <>
-      About me dawg.
-    </>
+    <div className={AboutCSS.root}>
+      <div
+        className={clsx(AboutCSS.section, AboutCSS.card)}
+      >
+        <p className={AboutCSS.luckyNumber}>138</p>
+        <p className={AboutCSS.name}>Michael</p>
+      </div>
+      <div className={clsx(AboutCSS.section, AboutCSS.information)}>
+        <div
+          className={AboutCSS.coin}
+        >
+        </div>
+        <div>
+          <div>
+            Printed on
+          </div>
+          <div>
+            11/14/2021 10:49
+          </div>
+        </div>
+      </div>
+    </div>
   )
 };
 
