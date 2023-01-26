3d1470a8ba4c24af73fb703732c5756f095136b6
Michael George <streetbard16@hotmail.com>
2023-01-25 21:47:16 -0800

    WIP: Setting up strapi for cms

---
+++ scripts/setupStrapi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This will pull the information from the env file.
+# STRAPI_DIR
+
+eval cat $(cat ../.env) docker run --rm -it \
+  -p 1337:1337 \
+  -v $(STRAPI_DIR):/srv/app \
+  strapi/strapi
