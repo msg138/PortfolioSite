cdb28a70120bb6fd8b49c6bbb79b229f685eb257
Michael George <streetbard16@hotmail.com>
2023-03-11 22:39:10 -0800

    Restyle website. Adding final touches

--- gatsby-browser.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Layout from "./src/modules/general/components/Layout";
-import { WrapPageElementBrowserArgs } from "gatsby";
-
-export const wrapPageElement = ({ element, props }: WrapPageElementBrowserArgs) => {
-  return <Layout {...props}>{element}</Layout>;
-};
